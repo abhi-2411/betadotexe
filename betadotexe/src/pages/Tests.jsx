78bdb4ea619690603c7d703e1efb92bf86d014ef
Abhishek Kutikuppala <abhishek24112005@gmail.com>
2025-03-10 21:49:37 +0530

    Added Tests.jsx

--- betadotexe/src/pages/Tests.jsx
+++ betadotexe/src/pages/Tests.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import "./Tests.css";
+import "./Sidebar.css"; 
 
-const Tests = () => {
+function Box({ title }) {
+  return <div className="box">{title}</div>;
+}
+
+function Tests() {
   return (
-    <div>
-      <h2>Tests</h2>
+    <div className="tests-container">
+      <Sidebar />
+      <div className="content-wrapper">
+        <header className="header">
+          <button className="notification-button">&#x1F514;</button>
+          <div className="profile-picture"></div>
+        </header>
+        <main className="main-content">
+          <div className="search-bar">
+            <button className="sign-in-button">+ Sign in with credentials</button>
+          </div>
+          <div className="boxes">
+            <Box title="Data Structures and Algorithms" />
+            <Box title="Artificial Intelligence" />
+            <Box title="Probability" />
+            <Box title="Text" />
+            <Box title="Text" />
+          </div>
+        </main>
+      </div>
     </div>
   );
-};
+}
 
 export default Tests;
